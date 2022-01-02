5ef3c06d3c5c907e14c31b5cf85cf7c805cdce1c
jakubwilk <jw@jakubwilk.pl>
2022-01-02 22:45:06 +0100

    Install antd UI library

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './app/App'
 import reportWebVitals from './reportWebVitals'
+import 'antd/dist/antd.compact.min.css'
 import './index.scss'
 
 ReactDOM.render(
