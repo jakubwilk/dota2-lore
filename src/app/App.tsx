3e785c16c4fd5ab676ecd1402d04213023940718
jakubwilk <jw@jakubwilk.pl>
2022-01-04 18:13:47 +0100

    Initialize react-router and create HomePage component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,11 @@
 import React from 'react'
-import logo from '../static/images/dota2_logo.png'
-import styles from './App.module.scss'
-import { Button } from 'antd'
+import { Routes, Route } from 'react-router-dom'
+import { HomePage } from './pages/home/Home'
 
 export const App = () => {
     return (
-        <div className={styles.App}>
-            <header className={styles.AppHeader}>
-                <img src={logo} className={styles.AppLogo} alt={'logo'} />
-                <p>
-                    {'Edit '}
-                    <code>{'src/App.tsx'}</code> {'and save to reload.'}
-                </p>
-                <a
-                    className={styles.AppLink}
-                    href={'https://reactjs.org'}
-                    target={'_blank'}
-                    rel={'noopener noreferrer'}
-                >
-                    {'Learn React'}
-                </a>
-                <Button size={'large'}>{'Antd Custom Button'}</Button>
-            </header>
-        </div>
+        <Routes>
+            <Route path={'/'} element={<HomePage />} />
+        </Routes>
     )
 }
