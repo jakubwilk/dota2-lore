a2880f0778a7a365c97eb4f9cc7f0fe7887c0b07
jakubwilk <jw@jakubwilk.pl>
2022-01-06 12:22:01 +0100

    Add new translation and rebuild main navigation

--- src/app/components/main-menu/MainMenu.tsx
+++ src/app/components/main-menu/MainMenu.tsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { appMenu } from '../../utils/constants/mainMenu'
-import styles from './MainMenu.module.scss'
-import { NavLink } from 'react-router-dom'
 import { Menu } from '../shared/menu/Menu'
 import { MenuItem } from '../shared/menu/MenuItem'
+import { appMenu } from '../../utils/constants/mainMenu'
+import styles from './MainMenu.module.scss'
 
 export const MainMenu = () => {
     const { t } = useTranslation()
 
     return (
-        <aside>
-            <Menu>
+        <aside className={styles.panel}>
+            <Menu className={styles.menu}>
                 {appMenu.map((menuSection, index) => (
                     <React.Fragment key={menuSection.key}>
-                        <MenuItem isEnable={true}>{t(menuSection.categoryKey)}</MenuItem>
+                        <MenuItem className={styles.menuSeparator} isEnable={true}>
+                            {t(menuSection.categoryKey)}
+                        </MenuItem>
                         {menuSection.menu.map((menuItem, index) => (
                             <MenuItem
                                 key={menuItem.key}
-                                className={'link'}
-                                linkClassName={'link'}
-                                activeLinkClassName={'link'}
+                                className={styles.menuItem}
+                                linkClassName={styles.menuLink}
+                                activeLinkClassName={styles.menuActiveLink}
                                 linkTarget={menuItem.href}
                                 linkTitle={t(menuItem.titleKey)}
                                 isEnable={menuItem.isEnable}
