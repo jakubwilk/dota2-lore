d1c0185470276f4fd9f551e4a2a07dde24af9aa6
jakubwilk <jw@jakubwilk.pl>
2022-01-09 19:03:22 +0100

    Create Footer component and add styling

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -8,21 +8,41 @@ import styles from './Footer.module.scss'
 
 export const Footer = ({}: IFooterProps) => {
     const { t } = useTranslation()
+    const currentYear: number = new Date().getFullYear()
 
     return (
-        <footer>
+        <footer className={styles.footer}>
             <Container isFullWidth={false} inheritContainerHeight={true}>
-                <Grid>
-                    <GridItem>
-                        <p>{'Wszelkie prawa zastrzeżone'}</p>
+                <Grid className={styles.row}>
+                    <GridItem className={styles.rowItem}>
+                        <p className={styles.text}>
+                            <a
+                                className={styles.link}
+                                href={'https://jakubwilk.pl'}
+                                title={'Przejdź do strony jakubwilk.pl'}
+                            >
+                                &copy; {currentYear} {'jakubwilk.pl'}
+                            </a>
+                            <br />
+                            {'Wszelkie prawa zastrzeżone'}
+                        </p>
                     </GridItem>
-                    <GridItem>
-                        <p>{'Projekt stworzony w oparciu o produkt:'}</p>
+                    <GridItem className={styles.rowItem}>
+                        <p className={styles.text}>
+                            {'Inspiracją do stworzenia strony była gra Dota2'}
+                            <br />
+                            {'opracowana i wydana przez Valve'}
+                        </p>
                         <a
+                            className={styles.link}
                             href={'https://www.valvesoftware.com/pl/'}
                             title={t('FooterLogoLinkTitleText')}
                         >
-                            <img src={valveLogo} alt={t('FooterLogoAltText')} />
+                            <img
+                                className={styles.logo}
+                                src={valveLogo}
+                                alt={t('FooterLogoAltText')}
+                            />
                         </a>
                     </GridItem>
                 </Grid>
