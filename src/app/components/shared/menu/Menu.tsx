265a5c9d1c04c2fff5b7b0d98b6f89d042539ef0
jakubwilk <jw@jakubwilk.pl>
2022-01-09 18:34:41 +0100

    Refactor custom menu component interface

--- src/app/components/shared/menu/Menu.tsx
+++ src/app/components/shared/menu/Menu.tsx
@@ -1,6 +1,45 @@
 import React from 'react'
-import { IMenuProps } from '../../../utils/interfaces/IMenu'
+import { IMenuProps, IMenuItemProps } from '../../../utils/interfaces/IMenu'
+import { NavLink } from 'react-router-dom'
 
 export const Menu = ({ className, children }: IMenuProps) => {
     return <ul className={className ?? ''}>{children}</ul>
+}
+
+export const MenuItem = ({
+    className,
+    linkClassName,
+    activeLinkClassName,
+    linkTitle,
+    linkTarget,
+    isDisable,
+    isEnable,
+    children,
+}: IMenuItemProps) => {
+    const isLink = (): boolean => {
+        if (linkClassName && activeLinkClassName && linkTarget) return true
+
+        return false
+    }
+
+    return isEnable ? (
+        <li className={className}>
+            {isLink() ? (
+                <NavLink
+                    className={(navData) =>
+                        navData.isActive
+                            ? `${linkClassName} ${activeLinkClassName}`
+                            : `${linkClassName}`
+                    }
+                    to={linkTarget as string}
+                    title={linkTitle}
+                    data-disable={isDisable}
+                >
+                    {children}
+                </NavLink>
+            ) : (
+                <React.Fragment>{children}</React.Fragment>
+            )}
+        </li>
+    ) : null
 }
